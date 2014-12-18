4499695bb8afbde1bbc2c922c6a370963fb64464
amiceli <miceli.antoine@gmail.com>
2014-12-18 17:16:06 +0100

    Create python script to test server with CURL with JSON example files for POST and PUT

---
+++ REST_test/test_server.py
@@ -0,0 +1,50 @@
+#!/user/bin/python
+
+import sys
+import os
+
+# -------------------------------------------------
+#
+# This script can be used for test server function
+# There are all commands for each reassources
+#
+# To use this script you can have to run this command : python test_server.pu arg1 arg2
+# arg1 is the HTTP method GET, POST, PUT or DELETE
+# arg2 is the ressources name
+# eg : python test_server.py GET conf returns all configurated inputs
+#
+# You have some JSON file with this script, these files containes some JSON data for POST and PUT method
+#
+# Enjoy ;)
+#
+# -------------------------------------------------
+
+if str(sys.argv[1]) == "GET":
+
+    # return all configurated inputs
+    if (str(sys.argv[2])) == "conf":
+        os.system('curl -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://localhost:5000/configurations')
+
+    # return all forms
+    elif (str(sys.argv[2])) == "form":
+        os.system('curl -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://localhost:5000/forms')
+
+    # return all keywords
+    elif (str(sys.argv[2])) == "keyword":
+        os.system('curl -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://localhost:5000/keywords')
+
+    # return all unities
+    elif (str(sys.argv[2])) == "unity":
+        os.system('curl -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://localhost:5000/unities')
+
+elif str(sys.argv[1]) == "POST":
+
+    # Create a configurated input with configuraedInput.json file data
+    if (str(sys.argv[2])) == "conf":
+        os.system(' curl -H "Content-Type: application/json" -d @JSON/configuratedInput.json http://localhost:5000/configurations')
+
+    # Create a form with form.json file data
+    elif (str(sys.argv[2])) == "form":
+        os.system(' curl -H "Content-Type: application/json" -d @JSON/form.json http://localhost:5000/form')
+else:
+    print("PUT or DELETE")
