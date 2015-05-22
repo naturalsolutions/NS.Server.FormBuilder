a7a150d2d74dc3ae857d00f587dd33589ae0df24
amiceli <miceli.antoine@gmail.com>
2015-05-22 14:44:13 +0200

    [#94488312] Fix autocomplete bugs, we added a controller for autocomplete ajax call. Now the autocomplete form name list is the same as form list

---
+++ project/controllers/AutocompleteController.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# 
+from project                    import app
+from ..models                   import session, Form
+
+import json
+
+# Return all forms
+@app.route('/autocomplete/forms', methods = ['GET'])
+def getFormName():
+    query = session.query(Form.name).all()
+
+    forms = []
+    for eachInput in query:
+        forms.append(eachInput[0])
+
+    return json.dumps({"options":forms})
--- project/controllers/__init__.py
+++ project/controllers/__init__.py
@@ -5,3 +5,4 @@ from . import UnityController
 from . import KeywordController
 from . import ConfiguratedFieldsController
 from . import LinkedFieldsController
+from . import AutocompleteController
