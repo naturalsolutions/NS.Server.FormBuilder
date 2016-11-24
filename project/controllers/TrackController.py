db2e49dc2f700f7acc172e34548d52600ab838dc
unknown <david_lassagne@natural-solutions.eu>
2016-11-24 15:34:52 +0100

    a few fixes to get unity values dynamically from track, simplified track connection

--- project/controllers/TrackController.py
+++ project/controllers/TrackController.py
@@ -11,30 +11,51 @@ import sys
 import datetime
 import pprint
 
+def getTrackSqlConnection():
+	with open ("project/config/config.json", "r") as myfile:
+		data = json.loads(myfile.read())
+
+	trackSqlConnexion = data["sql"]["urlTrack"] if 'sql' in data and 'urlTrack' in data['sql'] else abort(make_response('config.json file has no url referencing Track Database !', 400))
+	trackSqlConnexion = urllib.parse.quote_plus(trackSqlConnexion)
+	trackSqlConnexion = "mssql+pyodbc:///?odbc_connect=%s" % trackSqlConnexion
+
+	print("trackSqlConnexion = " + trackSqlConnexion)
+	try:
+		trackEngine = create_engine(trackSqlConnexion)
+	except e as Exception:
+		abort(make_response('Could not open database !', 400))
+	else:
+		return trackEngine
+	finally:
+		pass
+
+	return None
+
 @app.route('/Track/getData', methods = ['POST'])
 def getData():
 	if 'datas' in request.json:
 		toret = {}
-		with open ("project/config/config.json", "r") as myfile:
-			data = json.loads(myfile.read())
-		trackSqlConnexion = data["sql"]["urlTrack"] if 'sql' in data and 'urlTrack' in data['sql'] else abort(make_response('config.json file has no url referencing Track Database !', 400))
-		trackSqlConnexion = urllib.parse.quote_plus(trackSqlConnexion)
-		trackSqlConnexion = "mssql+pyodbc:///?odbc_connect=%s" % trackSqlConnexion
+		trackEngine = getTrackSqlConnection()
 
-		try:
-			trackEngine = create_engine(trackSqlConnexion)
-		except e as Exception:
-			abort(make_response('Could not open database !', 400))
-		else:
-
+		if (trackEngine != None):
 			for dataType in request.json["datas"]:
 				toret[dataType] = {}
 				result = trackEngine.execute("SELECT DISTINCT [TPro_"+dataType+"] FROM [TProtocole]")
 				for row in result:
 					toret[dataType][row[0]] = row[0]
-		finally:
-			pass
-
-		return json.dumps(toret, ensure_ascii=False)
+			return json.dumps(toret, ensure_ascii=False)
 	else:
 		abort(make_response('No datas given !', 400))
+
+@app.route('/Track/getTrackUnities/<string:lang>', methods = ['GET'])
+def getTrackUnities(lang):
+	toret = {}
+	trackEngine = getTrackSqlConnection()
+
+	if (trackEngine != None):
+		toret["unities"] = []
+		result = trackEngine.execute("SELECT DISTINCT [TUni_Label"+lang+"] FROM [TUnite] ORDER BY [TUni_Label"+lang+"]")
+		for row in result:
+			print(row)
+			toret["unities"].append(row[0])
+		return json.dumps(toret, ensure_ascii=False)
