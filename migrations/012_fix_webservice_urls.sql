e54814277ceba2f7ed25ac3f4e49e5ea2008b60d
rkjdid <romainknezevic@gmail.com>
2018-02-13 22:01:30 +0100

    fix crossdomain & erronous urls in db

---
+++ migrations/012_fix_webservice_urls.sql
@@ -0,0 +1,9 @@
+UPDATE InputProperty
+SET value = REPLACE(value, '127.0.0.1', 'track')
+WHERE name = 'webServiceURL'
+GO
+
+UPDATE InputProperty
+SET value = REPLACE(value, '/Thesaurus/', '/ThesaurusCore/')
+WHERE name = 'webServiceURL'
+GO
