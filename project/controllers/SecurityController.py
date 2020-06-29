e8b5de1d4808fb69c78d07d2a69c89c05d098665
Jeje2201 <jeremy_leriche@natural-solutions.eu>
2020-06-29 16:29:51 +0200

    Update SecurityController.py

--- project/controllers/SecurityController.py
+++ project/controllers/SecurityController.py
@@ -4,8 +4,8 @@ from project import app
 from flask import jsonify, abort, request, make_response
 import jwt
 
-@app.route('/Security/isCookieValid', methods = ['POST'])
-def isCookieValid():
+@app.route('/Security/haveToken', methods = ['POST'])
+def haveToken():
 	# this ensure request.json is read at least once.
 	# it turns out that IIS tries to be fancy: if you don't access request.json for reading
 	# on a POST request, it disregards any response from server and serves a 502 to client instead..
@@ -13,13 +13,13 @@ def isCookieValid():
 		abort(make_response('No json payload', 400))
 
 	if 'ecoReleve-Core' not in request.cookies:
-		abort(make_response('No reneco cookie in request', 400))
+		abort(make_response('No reneco cookie in request', 401))
 
 	if 'securityKey' not in request.json:
 		abort(make_response('No security key provided', 400))
 
 	try:
-		decoded = jwt.decode(request.cookies['ecoReleve-Core'], verify=False)
+		decoded = jwt.decode(request.cookies['ecoReleve-Core'], verify=True)
 	except:
 		abort(make_response('Error decoding reneco cookie', 400))
 
